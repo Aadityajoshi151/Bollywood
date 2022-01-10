8cff4121ea2e08aa03d52b1a21e043bde8a3f0fd
Aaditya Joshi <aadityajoshi151@gmail.com>
2022-01-10 12:19:31 +0530

    changed background color

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 490, minimizable: false,frame: false, maximizable: false, resizable: false, backgroundColor: '#455357' })
+  win = new BrowserWindow({ width: 800, height: 490, minimizable: false,frame: false, maximizable: false, resizable: false, backgroundColor: '#353638' })
   win.setMenu(null);
   // and load the index.html of the app.
   win.loadFile('index.html')
